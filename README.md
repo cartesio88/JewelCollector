06d1a68deb96b929348d12517890985f29c3b552
cesar <cesarmoracastro@gmail.com>
2012-09-13 01:35:54 -0500

    Erasing project

--- README.md
+++
@@ -1,4 +0,0 @@
-QNem
-====
-
-Android game 2D arcade. First experimental project of VC Developments
--- gen/com/vahancesar/HeroGame/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package com.vahancesar.HeroGame;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
